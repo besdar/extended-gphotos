63d5b44d2f146572d26549fb852bc4a31cb08488
besdar <besdar@protonmail.com>
2021-07-29 15:32:47 +0300

    Tests fix
    - incorrect coverage and work for App test was fixed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import App from './App';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 it('App do not render with wrong gallery structure function call', () => {
-  jest.mock('./Components/GalleryStructure.ts', () => ({ calculateGalleryStructureParams: () => null }));
-  const component = mount(<App />);
+  jest.mock('./Components/GalleryStructure', () => () => ({
+    calculateGalleryStructureParams: jest.fn().mockImplementation(() => null)
+  }));
 
-  expect(component).toMatchObject({});
+  const component = shallow(<App />);
+console.log('111');
+  expect(component.type()).toBeNull();
+
+  jest.unmock('./Components/GalleryStructure');
+});
+
+it('App renders with normal gallery structure function call', () => {
+  const component = shallow(<App />);
+  console.log('111');
+  expect(component.type()).not.toBeNull();
 });
